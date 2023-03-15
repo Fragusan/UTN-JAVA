a8bedf7e6f93a4186e2b0995c8ac2882ed13012e
fragusan <fragusan2@gmail.com>
2023-03-15 09:47:06 -0300

    add FrancoSort class & corregido FirstB

--- C2/FirstB.java
+++ C2/FirstB.java
@@ -1,58 +1,26 @@
 public class FirstB {
 
-    static int [] number(int x, int y, int z, String set){
-        int del[]= new int []{0};
-        int aux[]= new int[3];
-        int a=x;
-        int b=y;
-        int c=z;
-        int swap;
-        String orden= set;
-        orden=orden.toUpperCase();
 
-        if (orden.equals("DES")) {
-            System.out.println("Entro en descendientes");
-            //931 caso de orden inverso
-            if(a>b && a>c){
-                aux[2]=a;
-                if ( b>c) {
-                    aux[1]=b;
-                    aux[0]=c;
-                    System.out.println("quedo ordenado:");
-                    for (int i = 0; i <aux.length ; i++) {
-                        System.out.println(aux[i]);
-                    }
-
-                    return aux;
-                }
-            }
-            //857
-            if (a>b && a>c) {
-                aux[2]=a;
-                if (b<c) {
-                    aux[1]=c;
-                    aux[0]=b;
-                    System.out.println("quedo ordenado:");
-                    for (int i = 0; i <aux.length ; i++) {
-                        System.out.println(aux[i]);
-                    }
-                    return aux;
-                }
-            }
-        }
-        if (orden.equals("ASC")) {
-            System.out.println("Entro en ascendentes");
-        }
-        System.out.println("nada");
-        return del;
-    }
     public static void main(String[] args) {
-        System.out.println(number(3,4,8,"asc"));
 
-        System.out.println(number(6,1,8,"des"));
+        //en el caso de que ya este ordenado
+        int[] arr0= {3,4,8};
+        FrancoSort.sort(arr0);
 
-        System.out.println(number(9,3,1,"des"));
+        //se le pide en forma descendiente
+        int[] arr1= {6,1,8};
+        FrancoSort.sort(arr1,false);
 
-        System.out.println(number(8,5,7,"des"));
+        //se le pide en forma descendiente
+        int[] arr2= {9,1,3};
+        FrancoSort.sort(arr2,false);
+
+        //se le pide en forma descendiente
+        int[] arr3= {8,5,7};
+        FrancoSort.sort(arr3,false);
+
+        //Más heavy
+        int[] arr4 = { 5, 3, 8, 4, 2 };
+        FrancoSort.sort(arr4);
     }
 }
